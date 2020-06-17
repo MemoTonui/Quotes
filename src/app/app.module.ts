2dc54af26fd8f6f027935dfa36bbce8604d68d48
Linda Tonui <tonuilinda@gmail.com>
2020-06-17 15:36:05 +0300

    Add changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { from } from 'rxjs';
+import { QuoteCardComponent } from './quote-card/quote-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteFormComponent,
+    QuotesComponent,
+    QuoteCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
