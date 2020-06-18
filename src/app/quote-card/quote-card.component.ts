11713ac650af0f1f22e89b8324dafb09647b4010
Linda Tonui <tonuilinda@gmail.com>
2020-06-18 23:21:47 +0300

    Ahh the upvote and downvote functionality

--- src/app/quote-card/quote-card.component.ts
+++ src/app/quote-card/quote-card.component.ts
@@ -11,11 +11,12 @@ export class QuoteCardComponent implements OnInit {
 
    quotes:QuoteConstructor[]=[
      new QuoteConstructor ('Nelson Mandela','The greatest glory in living lies not in never falling, but in rising every time we fall.','Linda Tonui',1,0,new Date(2020,6,8)),
-     new QuoteConstructor ('Aristotle','It is during our darkest moments that we must focus to see the light.','Linda Tonui',0,0,new Date(2020,6,8)),
+     new QuoteConstructor ('Aristotle','It is during our darkest moments that we must focus to see the light.','Linda Tonui',10,0,new Date(2020,6,8)),
      new QuoteConstructor ('John Lennon','Life is what happens when you are busy making other plans.','Brian Yegon',0,0,new Date(2020,5,12)),
-     new QuoteConstructor ('Mother Teressa','Spread love everywhere you go. Let no one ever come to you without leaving happier.','Wachira Joan',0,0,new Date(2020,6,25))
+     new QuoteConstructor ('Mother Teressa','Spread love everywhere you go. Let no one ever come to you without leaving happier.','Wachira Joan',0,6,new Date(2020,6,25))
    ]
 
+   
    addNewQuote(QuoteConstructor){ 
     QuoteConstructor.timePassed = new Date();
     this.quotes.push(QuoteConstructor);
@@ -26,12 +27,7 @@ export class QuoteCardComponent implements OnInit {
    toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
-  addUpvotes(upvote,i) {
-    this.quotes[i].upvotes ++;
-  }
-  addDownvotes(downvote,i) {
-    this.quotes[i].downvotes  ++;
-  }
+ 
   deleteQuote(isComplete, index){
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].quoteWritter}?'s quote?`)
--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -9,16 +9,19 @@ import { QuoteConstructor } from '../quote-constructor';
 export class QuoteDetailsComponent implements OnInit {
 
   @Input() quote: QuoteConstructor;
-  @Output() upvote= new EventEmitter<boolean>();
-  @Output() downvote = new EventEmitter<boolean>();
+
   @Output() isComplete = new EventEmitter<boolean>();
 
-   addUpvote (add :true){
-     this.upvote.emit(add);
-   }
-   addDownvote (remove :true){
-     this.downvote.emit(remove);
-   }
+  upvotes=0;
+  downvotes = 0;
+
+  upVote(){
+    this.quote.upvotes ++
+  }
+  downVote(){
+    this.quote.downvotes ++
+  }
+
    quoteDelete(complete:boolean){
     this.isComplete.emit(complete);
   }
