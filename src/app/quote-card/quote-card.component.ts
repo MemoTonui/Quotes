46193904daf039fa06cf8dc314a4e12be9860591
Linda Tonui <tonuilinda@gmail.com>
2020-06-18 13:31:17 +0300

    Add Some changes

--- src/app/quote-card/quote-card.component.ts
+++ src/app/quote-card/quote-card.component.ts
@@ -10,12 +10,18 @@ import { Quote } from '@angular/compiler';
 export class QuoteCardComponent implements OnInit {
 
    quotes:QuoteConstructor[]=[
-     new QuoteConstructor ('Nelson Mandela','"The greatest glory in living lies not in never falling, but in rising every time we fall."','Linda Tonui'),
-     new QuoteConstructor ('Oprah Winfrey','"If you look at what you have in life, you will always have more. If you look at what you do not have in life, you will never have enough."','Linda Tonui')
+     new QuoteConstructor ('Nelson Mandela','"The greatest glory in living lies not in never falling, but in rising every time we fall."','Linda Tonui',0,0),
+     new QuoteConstructor ('Oprah Winfrey','"If you look at what you have in life, you will always have more. If you look at what you do not have in life, you will never have enough."','Linda Tonui',0,0)
    ]
 
    toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+  upvotes(i) {
+    this.quotes[i].upvotes ++;
+  }
+  downvotes(i) {
+    this.quotes[i].downvotes  ++;
   }
      
 
--- src/app/quote-constructor.ts
+++ src/app/quote-constructor.ts
@@ -1,6 +1,6 @@
 export class QuoteConstructor {
     showDescription :boolean;
-    constructor(public quoteWritter:string,public quote:string,public quotePoster:string){
+    constructor(public quoteWritter:string,public quote:string,public quotePoster:string,public upvotes:any,public downvotes:any){
         this.showDescription= false;
 
 
