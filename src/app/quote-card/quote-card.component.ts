1b32318f48d72091ae64d6bc98bb9c03effdbf1c
Linda Tonui <tonuilinda@gmail.com>
2020-06-18 11:22:59 +0300

    add a new component

--- src/app/quote-card/quote-card.component.ts
+++ src/app/quote-card/quote-card.component.ts
@@ -13,6 +13,10 @@ export class QuoteCardComponent implements OnInit {
      new QuoteConstructor ('Nelson Mandela','"The greatest glory in living lies not in never falling, but in rising every time we fall."','Linda Tonui'),
      new QuoteConstructor ('Oprah Winfrey','"If you look at what you have in life, you will always have more. If you look at what you do not have in life, you will never have enough."','Linda Tonui')
    ]
+
+   toggleDetails(index){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
      
 
   constructor() { }
